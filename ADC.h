388c7e6dd0ca4732fa59876d71928a9d898659c9
MCLEANS <jackagwaya@gmail.com>
2020-07-12 22:05:43 +0300

    Added struct to store channel results

--- ADC.h
+++ ADC.h
@@ -5,6 +5,25 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+struct ADC_channel_value{
+    uint16_t channel_0;
+    uint16_t channel_1;
+    uint16_t channel_2;
+    uint16_t channel_3;
+    uint16_t channel_4;
+    uint16_t channel_5;
+    uint16_t channel_6;
+    uint16_t channel_7;
+    uint16_t channel_8;
+    uint16_t channel_9;
+    uint16_t channel_10;
+    uint16_t channel_11;
+    uint16_t channel_12;
+    uint16_t channel_13;
+    uint16_t channel_14;
+    uint16_t channel_15;
+};
+
 class _ADC {
     private:
     private:
